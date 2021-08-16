dfefe4e19fec643c5b3c70b14bd8598083f4714c
mahesh <mail.mahesh.lk@gmail.com>
2021-08-16 15:19:57 +0800

    channel explore

--- chan2/channel2.go
+++ chan2/channel2.go
@@ -32,4 +32,9 @@ func main() {
 	// Consume the output.
 	fmt.Println(<-out) // 4
 	fmt.Println(<-out) // 9
+
+	// Set up the pipeline and consume the output.
+	for n := range sq(sq(sq(gen(2, 3)))) {
+		fmt.Println(n) // 16 then 81
+	}
 }
